6d242069087fadc47a8e60e2d3e46af65e64ea8a
Alaa Ayman <mdmamun907560@gmail.com>
2026-01-16 00:33:38 +0600

    Update script.js

--- script.js
+++ script.js
@@ -43,10 +43,18 @@ document.querySelectorAll('.magnetic').forEach(btn => {
     const rect = btn.getBoundingClientRect();
     const x = e.clientX - rect.left - rect.width / 2;
     const y = e.clientY - rect.top - rect.height / 2;
-    btn.style.transform = `translate(${x * 0.2}px, ${y * 0.2}px)`;
+    btn.style.transform = `translate(${x * 0.25}px, ${y * 0.25}px)`;
   });
 
   btn.addEventListener('mouseleave', () => {
     btn.style.transform = `translate(0,0)`;
   });
+});
+
+// Light follow
+const light = document.getElementById("light-follow");
+
+document.addEventListener("mousemove", e => {
+  light.style.left = e.clientX + "px";
+  light.style.top = e.clientY + "px";
 });
